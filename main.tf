d2d8644faa1cac4eebf54b2ff76ee657050526f4
Nishanth Pragnesh B <94343932+NishanthPragnesh@users.noreply.github.com>
2025-06-24 19:28:35 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,48 @@
+# 
+resource "aws_cloudfront_distribution" "static_cdn" {
+  origin {
+    domain_name = "${var.bucketname}.s3-website-us-east-1.amazonaws.com"
+    origin_id   = "s3Origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3Origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Project = "SampleStaticSite"
+  }
+}
